9732f45a9dfce4361e768e43e0ac7078933f4b06
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-09 01:36:07 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -9,5 +9,8 @@ body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' outputfile.txt)
 
 data="{\"title\":\"$title\",\"body\":\"$body\"}"
 
+echo -----------------
+echo $GITHUB_TOKEN
+
 #curl -i -H "Authorization: token $GITHUB_TOKEN" -d "$data" $issues_url
 curl -i -H "Authorization: token $secrets.GITHUB_TOKEN" -d "$data" $issues_url
