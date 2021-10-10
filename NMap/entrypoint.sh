026e4b7d5707559caaa877bc7750b393c95023a3
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-10 23:49:42 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -15,7 +15,19 @@ title="NMAP Scan on $(TZ=GST-1GDT date "+%D %T")"
 body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' outputfile.txt)
 body="servus\nkarli"
 #body=$(cat outputfile.txt)
-
+#-----------------------------------
+# Nmap 7.60 scan initiated Sun Oct 10 21:41:40 2021 as: nmap --oN outputfile.txt www.nivea.de
+#Nmap scan report for www.nivea.de (151.101.194.221)
+#Host is up (0.0054s latency).
+#Other addresses for www.nivea.de (not scanned): 151.101.130.221 151.101.66.221 151.101.2.221
+#Not shown: 998 filtered ports
+body=" \
+PORT    STATE SERVICE \
+80/tcp  open  http \
+443/tcp open  https \
+"
+# Nmap done at Sun Oct 10 21:41:57 2021 -- 1 IP address (1 host up) scanned in 17.53 seconds
+#-----------------------------------
 data="{\"title\":\"$title\",\"body\":\"$body\"}"
 
 #echo -----------------
