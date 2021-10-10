2e2a7d5e70af576eb304bf7f99211b4c79c73372
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-10 13:18:31 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -5,7 +5,6 @@ issues_url="https://api.github.com/repos/gitgrub/NMap-Action/issues"
 #nmap  --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -sV --open -iL /scan.txt --oN outputfile.txt
 nmap www.nivea.de --oN outputfile.txt
 
-
 # --oN    output normal
 # -sV     Attempts to determine the version of the service running on port
 # --open  Only show open (or possibly open) ports
@@ -14,18 +13,17 @@ nmap www.nivea.de --oN outputfile.txt
 
 title="NMAP Scan on $(date "+%D %T")"
 body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' outputfile.txt)
-
 body="servus"
+body=$(cat outputfile.txt)
 
 data="{\"title\":\"$title\",\"body\":\"$body\"}"
 
-echo -----------------
-echo $GH_TOKEN
-echo $TTEST
-echo -----------------
+#echo -----------------
+#echo $GH_TOKEN
+#echo $TTEST
+#echo -----------------
 
 #curl -i -H "Authorization: token $GITHUB_TOKEN" -d "$data" $issues_url
-#curl -i -H "Authorization: token $GH_TOKEN" -d "$data" $issues_url
 
 echo =cat========================================================================
 cat outputfile.txt
@@ -38,5 +36,4 @@ curl -X "POST" \
      -d "$data"
      
 #     -d '{"title":$title,"body":"huhu willi"}'
-     
 #     -H "Content-Type: text/plain; charset=utf-8" \
