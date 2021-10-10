e93e5d5c4cdf5660d22b6c3ff3434f1aff2f7107
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-10 14:25:34 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -11,8 +11,7 @@ nmap www.nivea.de --oN outputfile.txt
 # -iL     Scan targets from a file
 # -A
 
-TZ="Europe/Berlin"
-title="NMAP Scan on $(TZ=Europe/Berlin date "+%D %T")"
+title="NMAP Scan on $(TZ=:Europe/Berlin date "+%D %T")"
 body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' outputfile.txt)
 body="servus"
 #body=$(cat outputfile.txt)
