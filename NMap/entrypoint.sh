8ca5263529cbf27a039f7e4903c92931398970cc
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-11 00:05:51 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 issues_url="https://api.github.com/repos/gitgrub/NMap-Action/issues"
-
 #nmap  --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -sV --open -iL scan.txt --oN outputfile.txt
 #nmap  --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -sV --open -iL /scan.txt --oN outputfile.txt
 nmap www.nivea.de --oN outputfile.txt
-
 # --oN    output normal
 # -sV     Attempts to determine the version of the service running on port
 # --open  Only show open (or possibly open) ports
@@ -19,8 +17,8 @@ echo xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 body="servus\nkarli"
 #body=$(cat outputfile.txt)
 #-----------------------------------
-# Nmap 7.60 scan initiated Sun Oct 10 21:41:40 2021 as: nmap --oN outputfile.txt www.nivea.de
 body=" \
+# Nmap 7.60 scan initiated Sun Oct 10 21:41:40 2021 as: nmap --oN outputfile.txt www.nivea.de \n \
 Nmap scan report for www.nivea.de (151.101.194.221) \n \
 Host is up (0.0054s latency). \n \
 Other addresses for www.nivea.de (not scanned): 151.101.130.221 151.101.66.221 151.101.2.221 \n \
@@ -28,17 +26,11 @@ Not shown: 998 filtered ports \n \
 PORT    STATE SERVICE \n \
 80/tcp  open  http \n \
 443/tcp open  https \n \
+# Nmap done at Sun Oct 10 21:41:57 2021 -- 1 IP address (1 host up) scanned in 17.53 seconds \n \
 "
-# Nmap done at Sun Oct 10 21:41:57 2021 -- 1 IP address (1 host up) scanned in 17.53 seconds
 #-----------------------------------
+
 data="{\"title\":\"$title\",\"body\":\"$body\"}"
-
-#echo -----------------
-#echo $GH_TOKEN
-#echo $TTEST
-#echo -----------------
-
-#curl -i -H "Authorization: token $GITHUB_TOKEN" -d "$data" $issues_url
 
 echo =cat========================================================================
 echo =cat========================================================================
