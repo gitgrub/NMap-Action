ad044ea1250efb684b8a493a1f77e3f826f20e95
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-12 15:31:20 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 issues_url="https://api.github.com/repos/gitgrub/NMap-Action/issues"
+scan_url="www.nivea.de"
 #nmap  --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -sV --open -iL scan.txt --oN outputfile.txt
 #nmap  --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -sV --open -iL /scan.txt --oN outputfile.txt
-nmap www.nivea.de -A --oN outputfile.txt
+nmap $scan_url -A --oN outputfile.txt
 # --oN    output normal
 # -sV     Attempts to determine the version of the service running on port
 # --open  Only show open (or possibly open) ports
@@ -12,7 +13,7 @@ echo =cat outputfile.txt =======================================================
 cat outputfile.txt
 echo =cat========================================================================
 
-title="NMAP Scan on $(TZ=GST-1GDT date "+%D %T")"
+title="NMAP Scan $scan_url on $(TZ=GST-1GDT date "+%D %T")"
 #body=$(sed '1d;s/"/\\"/g;:a;N;$!ba;s/\n/\\n/g' outputfile.txt)
 p1="<pre>"
 p2="</pre>"
