a5afbaa459abf3014812524b835dd3e1d67376e2
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-12 15:12:02 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -47,7 +47,7 @@ echo ooooooooooooooooooooooooooooooooooooooooooooo
 body=$body1
 body=$(ls -la /usr/bin/md5*)
 
-body=$(sed ':a;N;$!ba;s/\n/\n\r/g' outputfile.txt)
+body=$(sed ':a;N;$!ba;s/\n/\r/g' outputfile.txt)
 
 
 data="{\"title\":\"$title\",\"body\":\"$body\"}"
