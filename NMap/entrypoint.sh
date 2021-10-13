13e6aabae59390e42c4d35c84bc82789f6834035
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-13 05:33:35 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -16,6 +16,8 @@ echo =cat=======================================================================
 hosts=$(sed ':a;N;$!ba;s/\n/, /g' /scan.txt)
 title="NMAP Scan *$hosts* on $(TZ=GST-1GDT LC_ALL=de_DE.utf8 date "+%D %T")"
 title="NMAP Scan *$hosts* on $(TZ=GST-1GDT LC_ALL=de_DE.utf8 date)"
+title="NMAP Scan *$hosts* on $(TZ=GST-1GDT LC_ALL=de_DE.utf8 date "+%y-%m-%d_%H:%M:%S")"
+
 p1="<pre>Scan ports: $ports<br><br>"
 p2="</pre>"
 body=$(sed ':a;N;$!ba;s/\n/<br>/g' outputfile.txt)
