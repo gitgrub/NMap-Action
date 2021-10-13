8986add8a2c79e3d0c5d2af0e44e897e83f58e53
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-13 03:29:48 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 issues_url="https://api.github.com/repos/gitgrub/NMap-Action/issues"
 scan_url="84.115.234.41"
+ports=$(cat /ports.txt)
 #nmap  --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -sV --open -iL /scan.txt --oN outputfile.txt
 #nmap $scan_url --oN outputfile.txt
-nmap --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt
+#nmap --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt
+nmap -p$ports -iL /scan.txt --oN outputfile.txt
 # --oN    output normal
 # -sV     Attempts to determine the version of the service running on port
 # --open  Only show open (or possibly open) ports
@@ -15,7 +17,7 @@ echo =cat=======================================================================
 
 hosts=$(sed ':a;N;$!ba;s/\n/, /g' /scan.txt)
 title="NMAP Scan *$hosts* on $(TZ=GST-1GDT date "+%D %T")"
-p1="<pre>"
+p1="<pre>scan ports: $ports<br>"
 p2="</pre>"
 body=$(sed ':a;N;$!ba;s/\n/<br>/g' outputfile.txt)
 body=$p1$body$p2
