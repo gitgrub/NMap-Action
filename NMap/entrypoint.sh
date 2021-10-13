f54cd19214e49eeeea7fdfbedb2ef2c1d1656f45
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-13 07:43:58 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -3,7 +3,7 @@ issues_url="https://api.github.com/repos/gitgrub/NMap-Action/issues"
 ports=$(cat /ports.txt)
 #nmap --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -sV --open -iL /scan.txt --oN outputfile.txt
 #nmap --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt
-nmap -v -O --open -p$ports -iL /scan.txt --oN outputfile.txt
+nmap -v -O --open -p$ports --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt
 # --oN    output normal
 # -sV     Attempts to determine the version of the service running on port
 # --open  Only show open (or possibly open) ports
@@ -11,12 +11,7 @@ nmap -v -O --open -p$ports -iL /scan.txt --oN outputfile.txt
 # -A
 
 hosts=$(grep -v '^#' /scan.txt)
-echo ----------------------------------------
-echo $hosts
-echo ----------------------------------------
 hosts=$(echo $hosts | sed 's/ /, /g')
-
-#hosts=$(sed ':a;N;$!ba;s/\n/, /g' /scan.txt)
 title="NMAP Scan *$hosts* on $(TZ=GST-1GDT date "+%d.%m.%Y %H:%M:%S")"
 
 p1="<pre>Scan ports: $ports<br><br>"
