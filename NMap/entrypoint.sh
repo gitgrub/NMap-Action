1178b86609da9c4b068cbb30da81418f87e0af62
gitgrub <509849+gitgrub@users.noreply.github.com>
2021-10-13 08:40:36 +0200

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -3,7 +3,9 @@ issues_url="https://api.github.com/repos/gitgrub/NMap-Action/issues"
 ports=$(cat /ports.txt)
 #nmap --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -sV --open -iL /scan.txt --oN outputfile.txt
 #nmap --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt
-nmap -v -O --open -p$ports --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt
+
+#nmap -v -O --open -p$ports --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt
+nmap -v -O --open --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt
 # --oN    output normal
 # -sV     Attempts to determine the version of the service running on port
 # --open  Only show open (or possibly open) ports
