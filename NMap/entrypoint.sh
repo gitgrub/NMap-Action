95009b0596c0645e5ef6e8a6e37f8b8e7694fbb0
gitgrub <509849+gitgrub@users.noreply.github.com>
2025-11-15 04:02:30 +0100

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -13,10 +13,10 @@ ports=$(cat /ports.txt)
 #nmap -v --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv --open -iL /scan.txt --oN outputfile.txt
 
 # test for no response - eh scho -Pn
-#nmap -v --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv --open -iL /scan.txt --oN outputfile.txt -Pn
+nmap -v --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt -Pn
 
 # test -np
-nmap -v -Pn -p 62977-62988 -iL /scan.txt --oN outputfile.txt
+#nmap -v -Pn -p 62977-62988 -iL /scan.txt --oN outputfile.txt
 
 # --oN    output normal
 # -sV     Attempts to determine the version of the service running on port
