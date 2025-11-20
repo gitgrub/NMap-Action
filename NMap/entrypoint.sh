5ec0c146d5afa59a79f01713b818bfc775feb7d8
gitgrub <509849+gitgrub@users.noreply.github.com>
2025-11-20 03:44:24 +0100

    Update entrypoint.sh

--- NMap/entrypoint.sh
+++ NMap/entrypoint.sh
@@ -16,6 +16,7 @@ ports=$(cat /ports.txt)
 #nmap -v --script nmap-vulners,vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt -Pn
 #nmap -v --script vulscan/vulscan --script-args vulscandb=exploitdb.csv -iL /scan.txt --oN outputfile.txt -Pn
 nmap -v -sV -Pn --script vulners -iL /scan.txt --oN outputfile.txt
+echo cpcpcpcpcpcpcpcpcpcpcpcpcpcpcpcpcpcpcpcpcpcpcp
 cp outputfile.txt /out.txt
 
 # test -np
